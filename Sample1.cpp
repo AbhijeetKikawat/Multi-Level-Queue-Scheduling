b8f5e35f293079a33db99b99fea62e97febb3791
AbhijeetKikawat <37634609+AbhijeetKikawat@users.noreply.github.com>
2018-04-10 00:26:45 +0530

    Update Sample1.cpp

--- Sample1.cpp
+++ Sample1.cpp
@@ -2,8 +2,9 @@
 int arrival_time1[30],arrival_time2[30],priority2[30],process2[30],arrival_time3[30];
 int burst_time1[30],burst_time2[30],burst_time3[30];
 
+int Total=0,t1=0,t2=0,t3=0;
+
 int n,i,at[30],bt[30],pr[30],j=0,k=0,l=0;
-//int time_quantum1,time_quantum2;
 
 int total,x,temp[30],counter=0;
 float avg_waiting_time1=0.0,avg_turnaround_time1=0.0;
@@ -16,6 +17,7 @@ float avg_waiting_time2,avg_turnaround_time2;
 
 void round_robin()
 {
+	printf("Time Quantum for Queue1 is 4\n");
 	for(i=0;i<j;i++)
 	{
 		temp[i]=burst_time1[i];
@@ -134,6 +136,44 @@ void fcfs()
     printf("\nAverage Turnaround Time=%f",avg_turnaround_time3);
 }
 
+void round_robin1()
+{
+	printf("Time Quantum between the 3 queues is 10\n");
+	for(i=1;i<Total;i=i+10)
+	{
+		if(t1>=10)
+		{
+			printf("Queue1 is running for 10 units\n");
+			t1=t1-10;
+		}
+		else if(t1<10&&t1!=0)
+		{
+			printf("Queue1 is running for %d units\n",t1);
+			t1=0;
+		}
+		if(t2>=10)
+		{
+			printf("Queue2 is running for 10 units\n");
+			t2=t2-10;
+		}
+		else if(t2<10&&t2!=0)
+		{
+			printf("Queue2 is running for %d units\n",t2);
+			t2=0;
+		}
+		if(t3>=10)
+		{
+			printf("Queue3 is running for 10 units\n");
+			t3=t3-10;
+		}
+		else if(t3<10&&t3!=0)
+		{
+			printf("Queue3 is running for %d units\n",t3);
+			t3=0;
+		}
+	}
+}
+
 int main()
 {
 	printf("Enter the no. of process you want to enter\n");
@@ -147,15 +187,17 @@ int main()
 		scanf("%d",&bt[i]);
 		printf("Priority(1 to 15):");
 		scanf("%d",&pr[i]);
+		Total=Total+bt[i];
 	}
 	for(i=0;i<n;i++)
 	{
 		if(pr[i]>=1&&pr[i]<=5)
 		{
-			printf("Process[%d] belongs to Queue 1\n",i+1);
+			printf("\n\nProcess[%d] belongs to Queue 1\n",i+1);
 			arrival_time1[j]=at[i];
 			burst_time1[j]=bt[i];
 			j++;
+			t1=t1+bt[i];
 		}
 		
 		else if(pr[i]>=6&&pr[i]<=10)
@@ -166,19 +208,23 @@ int main()
 			priority2[k]=pr[i];
 			process2[k]=k+1;
 			k++;
+			t2=t2+bt[i];
 		}
 		
 		else if(pr[i]>=11&&pr[i]<=15)
 		{
-			printf("Process[%d] belongs to Queue 3\n",i+1);
+			printf("Process[%d] belongs to Queue 3\n\n\n\n",i+1);
 			arrival_time3[l]=at[i];
 			burst_time3[l]=bt[i];
 			l++;
+			t3=t3+bt[i];
 		}
 	}
 	
+	round_robin1();
 	round_robin();
 	fcfs();
 	priority();
+	
 	return 0;
 }
